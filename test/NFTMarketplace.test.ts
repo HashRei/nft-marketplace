b74f9c701ec405a3e54ae90a26619405d1c9ecc6
HashRei <hashrei@protonmail.com>
2022-04-25 19:08:01 +0200

    Add tests for NFTMarkeplace

---
+++ test/NFTMarketplace.test.ts
@@ -0,0 +1,94 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { BigNumber, Contract, ContractFactory } from "ethers";
+
+let NFTMarketplace: ContractFactory;
+let nftMarketplace: Contract;
+let listingPrice: any;
+let auctionPrice: BigNumber;
+
+beforeEach(async function () {
+  /** Deploy core contracts **/
+
+  // Deploy the marketplace
+  NFTMarketplace = await ethers.getContractFactory("NFTMarketplace");
+  nftMarketplace = await NFTMarketplace.deploy();
+  await nftMarketplace.deployed();
+
+  listingPrice = await nftMarketplace.getListingPrice();
+  listingPrice = listingPrice.toString();
+
+  auctionPrice = ethers.utils.parseUnits("1", "ether"); // The actual price is in MATIC
+
+  // Checks
+  expect(listingPrice).to.equal(ethers.utils.parseUnits("1", "ether"));
+  expect(nftMarketplace.address).to.not.equal(ethers.constants.AddressZero);
+});
+
+describe("NFTMarket", function () {
+  it("Should create a token", async function () {
+    const token = await nftMarketplace.createToken(
+      "https://www.mytokenlocation.com", // Uniform Resource Identifier (URI) may point to JSON file that conforms to the ERC721 but not here
+      auctionPrice,
+      { value: listingPrice }
+    );
+
+    // Check
+    expect(token).to.not.equal(ethers.constants.AddressZero);
+  });
+
+  it("Should sell a token", async function () {
+    const [_, buyerAddress] = await ethers.getSigners(); // Here the first address is ignored thanks to the _
+
+    await nftMarketplace.createToken(
+      "https://www.mytokenlocation.com", // Uniform Resource Identifier (URI) may point to JSON file that conforms to the ERC721 but not here
+      auctionPrice,
+      { value: listingPrice }
+    );
+
+    const previousNFTMarketplaceState = await nftMarketplace.fetchMarketItems();
+
+    // Execute sale of token to the buyerAddress
+    await nftMarketplace
+      .connect(buyerAddress)
+      .createMarketSale(1, { value: auctionPrice });
+
+    //Checks
+    expect(previousNFTMarketplaceState).to.not.equal(
+      await nftMarketplace.fetchMarketItems()
+    );
+    expect(
+      await nftMarketplace.connect(buyerAddress).fetchMyNFTs()
+    ).to.not.equal(ethers.constants.AddressZero);
+  });
+
+  it("Should resell a token", async function () {
+    const [_, buyerAddress] = await ethers.getSigners(); // Here the first address is ignored thanks to the _
+
+    const token = await nftMarketplace.createToken(
+      "https://www.mytokenlocation.com", // Uniform Resource Identifier (URI) may point to JSON file that conforms to the ERC721 but not here
+      auctionPrice,
+      { value: listingPrice }
+    );
+
+    // execute sale of token to the buyerAddress
+    await nftMarketplace
+      .connect(buyerAddress)
+      .createMarketSale(1, { value: auctionPrice });
+
+    const tokenSecondState = await nftMarketplace
+      .connect(buyerAddress)
+      .fetchMyNFTs();
+
+    // Resell a token
+    await nftMarketplace
+      .connect(buyerAddress)
+      .resellToken(1, auctionPrice, { value: listingPrice });
+
+    // Checks
+    expect(await nftMarketplace.fetchMarketItems()).to.not.equal(token);
+    expect(await nftMarketplace.fetchMarketItems()).to.not.equal(
+      tokenSecondState
+    );
+  });
+});
