b9f5957fb030e601662e103c8a195dd56a20b45d
HashRei <hashrei@protonmail.com>
2022-04-29 11:36:24 +0200

    Add search bar to navbar

--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -5,6 +5,13 @@ import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import { ethers } from "ethers";
 
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+
 let web3Modal: Web3Modal;
 
 const providerOptions = {
@@ -32,7 +39,7 @@ export function Navbar() {
     if (typeof window.ethereum !== "undefined") {
       setHasMetamask(true);
     }
-  });
+  }, []);
 
   async function connect() {
     if (typeof window.ethereum !== "undefined") {
@@ -53,28 +60,56 @@ export function Navbar() {
     <div className="fixed top-0 left-0 box-border flex h-16 w-full max-w-full items-center justify-between break-words border-0 border-solid border-white leading-6 text-slate-100 md:h-20">
       <div className="flex p-2 space-x-3">
         {/* The Image below generates "Warning: Prop `style` did not match" an erro in the console */}
-        {/* <Image
+        <Image
           src="/Logo_LooksSea.png"
           alt="LooksSea logo"
           width={"40px"}
           height={"40px"}
-        /> */}
+        />
         <p className="font-bold text-4xl">LooksSea</p>
       </div>
+      <Paper
+        component="form"
+        sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+      >
+        <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search LooksSea"
+          inputProps={{ "aria-label": "search looksSea" }}
+        />
+      </Paper>
       <Link href="/">My collection</Link>
       <Link href="/">Mint and sell</Link>
 
       {/* MY WALLET */}
 
-      <div>
+      <div className="mr-3">
         {hasMetamask ? (
           isConnected ? (
-            "Connected! "
+            <button className="disabled:opacity-70  disabled:cursor-not-allowed px-10 py-3 border rounded-md text-base font-bold font-sans mt-2">
+              Connected
+            </button>
           ) : (
-            <button onClick={() => connect()}>Connect</button>
+            <button
+              className="disabled:opacity-70  disabled:cursor-not-allowed px-10 py-3 border rounded-md text-base font-bold font-sans mt-2"
+              onClick={() => connect()}
+            >
+              Connect Metamask
+            </button>
           )
         ) : (
-          "Please install metamask"
+          <button
+            className="disabled:opacity-70  disabled:cursor-not-allowed px-10 py-3 border rounded-md text-base font-bold font-sans mt-2"
+            onClick={() =>
+              window.open("https://metamask.io/download", "_blank")
+            }
+          >
+            Install Metamask
+          </button>
         )}
 
         {isConnected ? <button>Execute</button> : ""}
