5e8d97516038c11358580faf3516d6b7f730ab52
HashRei <hashrei@protonmail.com>
2022-04-29 09:26:26 +0200

    Remove main html element from idex.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,13 @@
-import type { NextPage } from 'next'
-import { Page } from '../components/Page'
-import { Navbar } from '../components/sections/Navbar'
+import type { NextPage } from "next";
+import { Page } from "../components/Page";
+import { Navbar } from "../components/sections/Navbar";
 
 const Home: NextPage = () => {
   return (
-    
     <Page>
-      <main className="main">
-        <Navbar/>
-      </main>
+      <Navbar />
     </Page>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
