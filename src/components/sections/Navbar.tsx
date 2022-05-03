d469e1fd782dea48d001ea63f67fc81ae0e0eb54
HashRei <hashrei@protonmail.com>
2022-05-03 10:25:26 +0200

    Add responsive version of the navbar

--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -1,9 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import Web3Modal from "web3modal";
-import WalletConnectProvider from "@walletconnect/web3-provider";
-import { ethers } from "ethers";
+import { Wallet } from "../../blockchain/Wallet";
 
 import * as React from "react";
 import Paper from "@mui/material/Paper";
@@ -11,109 +8,71 @@ import InputBase from "@mui/material/InputBase";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
-
-let web3Modal: Web3Modal;
-
-const providerOptions = {
-  walletconnect: {
-    package: WalletConnectProvider, // required
-    options: {
-      rpc: { 137: process.env.NEXT_PUBLIC_RPC_URL }, // required
-    },
-  },
-};
-
-if (typeof window !== "undefined") {
-  web3Modal = new Web3Modal({
-    cacheProvider: false,
-    providerOptions, // required
-  });
-}
-
 export function Navbar() {
-  const [isConnected, setIsConnected] = useState(false);
-  const [hasMetamask, setHasMetamask] = useState(false);
-  // const [signer, setSigner] = useState(undefined);
-
-  useEffect(() => {
-    if (typeof window.ethereum !== "undefined") {
-      setHasMetamask(true);
-    }
-  }, []);
-
-  async function connect() {
-    if (typeof window.ethereum !== "undefined") {
-      try {
-        const web3ModalProvider = await web3Modal.connect();
-        setIsConnected(true);
-        const provider = new ethers.providers.Web3Provider(web3ModalProvider);
-        // setSigner(provider.getSigner());
-      } catch (e) {
-        console.log(e);
-      }
-    } else {
-      setIsConnected(false);
-    }
-  }
-
   return (
-    <div className="fixed top-0 left-0 box-border flex h-16 w-full max-w-full items-center justify-between break-words border-0 border-solid border-white leading-6 text-slate-100 md:h-20">
-      <div className="flex p-2 space-x-3">
-        {/* The Image below generates "Warning: Prop `style` did not match" an erro in the console */}
-        <Image
-          src="/Logo_LooksSea.png"
-          alt="LooksSea logo"
-          width={"40px"}
-          height={"40px"}
-        />
-        <p className="font-bold text-4xl">LooksSea</p>
-      </div>
-      <Paper
-        component="form"
-        sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
-      >
-        <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
-          <SearchIcon />
-        </IconButton>
-        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="Search LooksSea"
-          inputProps={{ "aria-label": "search looksSea" }}
-        />
-      </Paper>
-      <Link href="/">My collection</Link>
-      <Link href="/">Mint and sell</Link>
+    <section>
 
-      {/* MY WALLET */}
+      {/* DesktopNavbar */}
 
-      <div className="mr-3">
-        {hasMetamask ? (
-          isConnected ? (
-            <button className="disabled:opacity-70  disabled:cursor-not-allowed px-10 py-3 border rounded-md text-base font-bold font-sans mt-2">
-              Connected
-            </button>
-          ) : (
-            <button
-              className="disabled:opacity-70  disabled:cursor-not-allowed px-10 py-3 border rounded-md text-base font-bold font-sans mt-2"
-              onClick={() => connect()}
-            >
-              Connect Metamask
-            </button>
-          )
-        ) : (
-          <button
-            className="disabled:opacity-70  disabled:cursor-not-allowed px-10 py-3 border rounded-md text-base font-bold font-sans mt-2"
-            onClick={() =>
-              window.open("https://metamask.io/download", "_blank")
-            }
+      <div className=" hidden desktop:flex fixed flex-col w-full items-center space-y-3 desktop:flex-row justify-between  ">
+        <div className="flex p-2 space-x-3">
+          {/* The Image below generates "Warning: Prop `style` did not match" an erro in the console */}
+          <Image
+            src="/Logo_LooksSea.png"
+            alt="LooksSea logo"
+            width={"40px"}
+            height={"40px"}
+          />
+          <p className="font-bold text-4xl">LooksSea</p>
+        </div>
+        <div className="display-mobile">
+          <Paper
+            component="form"
+            sx={{
+              p: "2px 4px",
+              display: "flex",
+              alignItems: "center",
+              width: 600,
+            }}
           >
-            Install Metamask
-          </button>
-        )}
+            <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+              <SearchIcon />
+            </IconButton>
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Search LooksSea"
+              inputProps={{ "aria-label": "search looksSea" }}
+            />
+          </Paper>
+        </div>
 
-        {isConnected ? <button>Execute</button> : ""}
+        <Link href="/">My collection</Link>
+        <Link href="/">Mint and sell</Link>
+
+        <Wallet />
       </div>
-    </div>
+
+      {/* MobileNavbar */}
+
+      <div className=" fixed flex-col w-full items-center space-y-3 flex justify-between desktop:hidden ">
+        <div className="tablet:flex tablet:justify-around tablet:w-full">
+          <div className="flex p-2 space-x-3">
+            {/* The Image below generates "Warning: Prop `style` did not match" an erro in the console */}
+            <Image
+              src="/Logo_LooksSea.png"
+              alt="LooksSea logo"
+              width={"40px"}
+              height={"40px"}
+            />
+            <p className="font-bold text-4xl">LooksSea</p>
+          </div>
+          <Wallet />
+        </div>
+
+        <Link href="/">My collection</Link>
+        <Link href="/">Mint and sell</Link>
+      </div>
+    </section>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      screens: {
+        tablet: '640px',
+        laptop: '1024px',
+        desktop: '1280px'
+      },
+    }
   },
   plugins: []
 }
