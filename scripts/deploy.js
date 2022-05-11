8005b92ad17deda95bd53042864c1f66505742a2
HashRei <hashrei@protonmail.com>
2022-05-11 16:44:00 +0200

    Add deploy script and remove old sample script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,20 +6,14 @@
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
+  const nftMarketplace = await NFTMarketplace.deploy();
 
-  await greeter.deployed();
+  await nftMarketplace.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("NFTMarketplace deployed to:", nftMarketplace.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
