d9c5b523a686307e0173c506a8be96a8a47d1f32
HashRei <hashrei@protonmail.com>
2022-05-13 11:28:25 +0200

    Update MyAsssets NFT container and price display

--- src/components/MyAssets.tsx
+++ src/components/MyAssets.tsx
@@ -64,32 +64,39 @@ export default function MyAssets() {
   if (loadingState == "loaded" && !nfts.length)
     return <h1 className="py-10 px-20 text-3xl">No NFTs owned</h1>;
   return (
-    <div className="flex justify-center">
-      <div className="p-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-          {nfts.map((nft:any, i) => (
-            <div key={i} className="border shadow rounded-xl overflow-hidden">
-              <Image
-                className="rounded"
-                src={nft.image}
-                alt="NFT file"
-                width={"350px"}
-                height={"350px"}
-              />
-              <div className="p-4 bg-black">
-                <p className="text-2xl font-bold text-white">
-                  Price - {nft.price} Eth
-                </p>
-                <button
-                  className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
-                  onClick={() => listNFT(nft)}
-                >
-                  List
-                </button>
+    <div className="flex justify-center p-4">
+      <div className="grid grid-cols-1 laptop:grid-cols-2 desktop:grid-cols-4 gap-4 pt-4">
+        {nfts.map((nft: any, i) => (
+          <div key={i} className="border shadow rounded-xl overflow-hidden">
+            <Image
+              className="rounded"
+              src={nft.image}
+              alt="NFT file"
+              width={350}
+              height={257}
+              objectFit="cover"
+              quality={100}
+            />
+            <div className="p-4 bg-black">
+              <div className="flex gap-2 text-2xl font-bold text-white">
+                Price - {nft.price}{" "}
+                <Image
+                  src="/Polygon-Matic-Logo.svg"
+                  alt="Polygon Matic Logo"
+                  width={25}
+                  height={25}
+                />
+                MATIC
               </div>
+              <button
+                className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
+                onClick={() => listNFT(nft)}
+              >
+                List
+              </button>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   );
